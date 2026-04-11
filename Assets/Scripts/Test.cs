837314461391fb9351d7edc4937692ff96f51891
YANG251767 <3342454349@qq.com>
2026-04-11 16:12:23 +0800

    test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int health;
+    public int ATK;
+    public string name;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = 100;
+        ATK = 10;
+        name = "Player";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
